e1a86ad4860ab9f84f13dd09f465f4d29a3f6eb9
AlessandroMeschi <91841733+AlessandroMeschi@users.noreply.github.com>
2021-11-29 21:48:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  
+  char a=a;
+  cin >> a;
+  
+  if(conversione(a) == true){
+    cout << a <<endl;
+  }else{
+    cout << "errore" <<endl;
+  }
   return 0;
 }
